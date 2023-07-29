8d252441c14228e81c1995c275d38229d7ba5b35
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-07-29 10:19:29 +0300

    display bots using map

--- src/Components/BotColllection.jsx
+++ src/Components/BotColllection.jsx
@@ -1,20 +1,35 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 function BotCollection() {
     const [bots, setBots] = useState([])
     
     useEffect(() => {
-        fetch('http://localhost:3000/bots')
-            .then((res) => res.json())
-            .then((data) => {
-                setBots(data)
-            })
+        fetch('http://localhost:3000/bots').then((res) => res.json()).then((data) => {
+            setBots(data)
+        })
     }, [])
-    
-    console.log(bots)
 
+    const displayBots = bots.map(({ name, health, damage, armor, catchphrase, avatar_url }) => {
+        return (
+            <div className="col">
+                <div className="card h-100">
+                    <img src={avatar_url} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">{catchphrase}</p>
+                    </div>
+                    <div className="card-footer">
+                        <small className="text-muted">{health}, {damage}, {armor}</small>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+    
     return (
-        <div>botcollection</div>
+        <div class="row row-cols-1 row-cols-md-4 g-3">
+            {displayBots}
+        </div>
     )
 }
 
