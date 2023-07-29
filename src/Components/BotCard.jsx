97e52c88f45368972212e493e7be61a2199d238e
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-07-29 20:51:57 +0300

    change onClick to be on the main details div of card

--- src/Components/BotCard.jsx
+++ src/Components/BotCard.jsx
@@ -4,19 +4,24 @@ function BotCard({ bot, handleBotClick }) {
     const { id, name, health, damage, armor, catchphrase, avatar_url } = bot;
 
     return (
-        <div key={id} className="col bot-card" onClick={() => { handleBotClick(id) }}>
+        <div key={id} className="col bot-card">
             <div className="card h-100">
                 {/* X mark icon styling */}
                 <div style={{ display: "flex", justifyContent: "right" }}>
-                    <HiXMark color="red" size="2rem" stroke-width="1"  />
+                    <div className="xmark-box">
+                        <HiXMark color="red" size="2rem" stroke-width="1"  />
+                    </div>
                 </div>
-                <img src={avatar_url} className="card-img-top" alt="..."/>
-                <div className="card-body">
-                    <h5 className="card-title">{name}</h5>
-                    <p className="card-text">{catchphrase}</p>
-                </div>
-                <div className="card-footer">
-                    <small className="text-muted">{health}, {damage}, {armor}</small>
+
+                <div className="card-main-details" onClick={() => { handleBotClick(id) }}>
+                    <img src={avatar_url} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{name}</h5>
+                        <p className="card-text">{catchphrase}</p>
+                    </div>
+                    <div className="card-footer">
+                        <small className="text-muted">{health}, {damage}, {armor}</small>
+                    </div>
                 </div>
             </div>
         </div>
