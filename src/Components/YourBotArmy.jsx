bde9ed5f5ca14f798ca6ca6fc65df7865c0e733b
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-07-29 14:16:07 +0300

    give bot army fixed height

--- src/Components/YourBotArmy.jsx
+++ src/Components/YourBotArmy.jsx
@@ -2,7 +2,7 @@ function YourBotArmy({ botArmy }) {
     
     const displayBotArmy = botArmy.map(({ id, name, health, damage, armor, catchphrase, avatar_url }) => {
         return (
-            <div className="col">
+            <div key={id} className="col">
                 <div className="card h-60">
                     <img src={avatar_url} className="card-img-top" alt="..."/>
                     <div className="card-body">
@@ -20,7 +20,7 @@ function YourBotArmy({ botArmy }) {
     return (
         <div className="p-10">
             <h3>Bot Army</h3>
-            <div className="row row-cols-1 row-cols-md-5 g-3 m-4 pb-3 px-3 bot-army">
+            <div className="row row-cols-1 row-cols-md-5 g-3 m-4 pb-3 h-100 px-3 bot-army">
                 {displayBotArmy}
             </div>
         </div>
