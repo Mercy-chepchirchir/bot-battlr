f7bde045d420b128d6c3ff0e47ccb3780343f104
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-07-29 12:31:51 +0300

    map display bot army

--- src/Components/YourBotArmy.jsx
+++ src/Components/YourBotArmy.jsx
@@ -1,21 +1,27 @@
 function YourBotArmy({ botArmy }) {
+    
+    const displayBotArmy = botArmy.map(() => {
+        return (
+            <div className="col">
+                <div className="card h-60">
+                    <img src="https://robohash.org/nostrumrepellendustenetur.png?size=300x300&set=set1" className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title"></h5>
+                        <p className="card-text">aasdf</p>
+                    </div>
+                    <div className="card-footer">
+                        <small className="text-muted">65, 70, 47</small>
+                    </div>
+                </div>
+            </div>
+        )
+    })
 
     return (
         <div className="p-10">
             <h3>Bot Army</h3>
             <div className="row row-cols-1 row-cols-md-5 g-3 m-4 pb-3 px-3 bot-army">
-                <div className="col">
-                    <div className="card h-60">
-                        <img src="https://robohash.org/nostrumrepellendustenetur.png?size=300x300&set=set1" className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">Hila</h5>
-                            <p className="card-text">aasdf</p>
-                        </div>
-                        <div className="card-footer">
-                            <small className="text-muted">65, 70, 47</small>
-                        </div>
-                    </div>
-                </div>
+                {displayBotArmy}
             </div>
         </div>
     )
