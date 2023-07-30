b451d9abc13184e69a9149ce408eb08e675b9a2e
mercy-chepchirchir <mercychepchirchir33@gmail.com>
2023-07-31 02:03:45 +0300

    find the current bot and display it's details instead of hard-coded details

--- src/Components/BotSpecs.jsx
+++ src/Components/BotSpecs.jsx
@@ -11,6 +11,13 @@ function BotSpecs({ botArmy, originalBots, setBotArmy}) {
     // for example from https://localhost:5173/105  where 105 is currentBotId
     const { currentBotId } = useParams();
 
+    const currentBot = originalBots.find((oneBot) => {
+        // convert currentBotId to integer from string and return the bot in collection matching the id
+        return oneBot.id === parseInt(currentBotId)
+    })
+
+    const { name, catchphrase, avatar_url, bot_class , health, damage, armor } = currentBot;
+
     // this function was moved here from BotCollection compoenent instead so that when enlist button is clicked, it adds bot to army
     const handleEnlistBtnClick = (botId) => {
         // convert botId from url path, string to an integer
@@ -41,18 +48,18 @@ function BotSpecs({ botArmy, originalBots, setBotArmy}) {
 
             <div className="card-main-details d-flex justify-content-center">
                 <div className="left-details-part">
-                    <img src="https://robohash.org/quidemconsequaturaut.png?size=300x300&set=set1" className="card-img-top" alt="..."/>
+                    <img src={avatar_url} className="card-img-top" alt="..."/>
                 </div>
 
                 <div className="right-details-part text-left">
-                    <h4>Name: u-69</h4>
+                    <h4>Name: {name}</h4>
                     <div className="card-body">
                         <h5 className="card-title">Catchphrase</h5>
-                        <p className="card-text">0000000000185028520385023</p>
+                        <p className="card-text">{catchphrase}</p>
                     </div>
-                    <h5 className="card-title">Class: Assault</h5>
+                    <h5 className="card-title">Class: {bot_class}</h5>
                     <div className="card-footer">
-                        <FaHeartbeat/> 23 <BsFillLightningFill/> 53 <BsShieldShaded /> 52
+                        <FaHeartbeat/> {health} <BsFillLightningFill/> {damage} <BsShieldShaded /> {armor}
                     </div>
                     <Link to="/" className="btnGoBack d-block my-3 w-100">Go Back</Link>
                     {/* add onClick handler to enlist button which will add bot to army when clicked */}
